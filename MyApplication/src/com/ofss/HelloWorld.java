5fb25e2e0b4f7a6e4d82272c4ee54ee5fa8e8042
pysharma <pysharma@PYSHARMA-LAP.in.oracle.com>
2019-04-16 12:06:25 +0530

    commit

--- MyApplication/src/com/ofss/HelloWorld.java
+++ MyApplication/src/com/ofss/HelloWorld.java
@@ -3,7 +3,7 @@ package com.ofss;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.print("Pushing to depo server");
+		System.out.print("Pushing to depo server by Prashant");
 
 	}
 
